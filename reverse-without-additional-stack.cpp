db9666d6841615d6048c51cec3da9233dd2d068e
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-03-09 23:37:13 +0530

    Add files via upload

---
+++ reverse-without-additional-stack.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+void insertAtBottom(stack<int> &st,int ele){
+    
+    if(st.empty()){
+        st.push(ele);
+        return;
+    }
+    
+    int top_ele=st.top();
+    st.pop();
+    insertAtBottom(st,ele);
+    st.push(top_ele);
+}
+
+void reverse(stack<int> &st){
+    
+    if(st.empty()){
+        return;
+    }
+    
+    int ele=st.top();
+    st.pop();
+    reverse(st);
+    insertAtBottom(st,ele);
+}
+
+int main()
+{
+    stack<int> st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+   
+   cout<<"Stack before reversing:\t";
+   while(!st.empty()){
+       cout<<st.top()<<" ";
+       st.pop();
+   }cout<<endl;
+
+
+    
+    
+        st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    
+    reverse(st);
+    
+    cout<<"Stack after reversing:\t"<<endl;
+       while(!st.empty()){
+       cout<<st.top()<<" ";
+       st.pop();
+   }cout<<endl;
+
+    
+    return 0;
+}
